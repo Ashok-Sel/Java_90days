c2628633cd00fa02d20c4e35dce0a604d8949e03
Ashok-Sel <65298102+Ashok-Sel@users.noreply.github.com>
2020-07-01 18:18:53 +0530

    Add files via upload
    java programs uploaded

---
+++ ninentydays/CharOccurrence.java
@@ -0,0 +1,36 @@
+package javascripts;
+
+public class CharOccurrence {
+
+	public static void main(String[] args) {
+		
+		// The program is to find the number of occurrence of a char in the String 
+		
+		// One method is to convert to char array and looping through it 
+		String text = "You have no choice other than following me!"; 
+		
+		char[] ch = text.toCharArray(); 
+		
+		int count = 0;  
+		
+		for (int i = 0; i < ch.length; i++) { 
+			if (ch[i] == 'o') {
+				count++; 
+			}
+		} 
+		
+		System.out.println("Number of occurrence of o in the given text: " + count);
+		
+		// Using Regex [^o] to replace the char and taking the count 
+		
+		String Str = "You have no choice other than following me!";
+		String replacedStr = Str.replaceAll("[^o]", ""); 
+		System.out.println("\nNumber of occurrence of o is: " + replacedStr.length());
+		
+		
+		// Another method is we can use HashMap to take the count of the desired character  
+		
+
+	}
+
+}
